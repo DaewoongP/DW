1d62f578e612143165b84f671ee161d63f2efc2f
safecorners <safeconers.mobile@gmail.com>
2018-07-26 15:40:27 +0900

    #40 add a argument parser

--- device/main.py
+++ device/main.py
@@ -1,1 +1,17 @@
-from ptzmodule import ptmodule
+import argparse
+
+
+parser = argparse.ArgumentParser(description="PtzCamera")
+parser.add_argument('-b', '--broker-url', default="localhost:1883", type=str,
+                    metavar='PATH', help='mqtt broker address (default: localhost:1883')
+parser.add_argument('-o', '--ffserver-url', default='http://localhost:8090/feed1.ffm', type=str,
+                    metavar='PATH', help='url used to set up rtsp streaming')
+
+
+def main():
+    global args
+    args = parser.parse_args()
+    print(args)
+
+if __name__ == '__main__':
+   main()
