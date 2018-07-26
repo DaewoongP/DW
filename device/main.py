3b79b6a5c11d811f54dd92c7d8896166447cb731
safecorners <safeconers.mobile@gmail.com>
2018-07-26 16:52:19 +0900

    #40 subprocess: exec_ffmpeg

--- device/main.py
+++ device/main.py
@@ -1,17 +1,56 @@
 import argparse
+import subprocess as sp
 
+import paho.mqtt.client as paho
+
+import json
+import time
 
 parser = argparse.ArgumentParser(description="PtzCamera")
-parser.add_argument('-b', '--broker-url', default="localhost:1883", type=str,
-                    metavar='PATH', help='mqtt broker address (default: localhost:1883')
-parser.add_argument('-o', '--ffserver-url', default='http://localhost:8090/feed1.ffm', type=str,
+parser.add_argument('-b', '--broker-url', default="safecorners.io:1883", type=str,
+                    metavar='PATH', help='mqtt broker address')
+parser.add_argument('-o', '--output-url', default='http://localhost:8090/feed1.ffm', type=str,
                     metavar='PATH', help='url used to set up rtsp streaming')
+
+
+def execute_ffmpeg():
+    command = ['ffmpeg',
+               '-f', 'video4linux2',
+               '-i', '/dev/video0',
+               '-vcodec', 'libx264',
+               '-s', '640x480',  # size of one frame
+               '-r', '30',  # frames per second
+               '-biv', '150k',  # bit per rate
+               '-bufsize', '40'  # buffer size
+               '-an',  # Tells FFMPEG not to expect any audio
+               'test.mp4']
+
+    pipe = sp.Popen(command, stdin=sp.PIPE, stderr=sp.PIPE)
+
+
+def system_message(object, userdata, msg):
+    print("{}: {}".format(msg.topic, msg.payload.decode('utf-8')))
 
 
 def main():
     global args
     args = parser.parse_args()
-    print(args)
+
+    # Connect
+    client = paho.Client()
+    host, port = args.broker_url.split(':')
+    client.connect(host, int(port))
+
+    # subscribe a system messages
+    client.message_callback_add("SYS/#", system_message)
+    client.subscribe("SYS/#")
+
+    # Instantiate ptz module
+
+    # Streaming
+    execute_ffmpeg()
+
+    client.loop_forever()
 
 if __name__ == '__main__':
-   main()
+    main()
